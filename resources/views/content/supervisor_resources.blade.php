8083b9fa0eceaa99bbd71888761b5158b3fb3f1f
soilsci <jane.hawkins@rothamsted.ac.uk>
2025-01-24 13:52:08 +0000

    added supervisor resourcepage

---
+++ resources/views/content/supervisor_resources.blade.php
@@ -0,0 +1,52 @@
+<x-guest-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Information for Supervisors and Programme Directors') }}
+        </h2>
+    </x-slot>
+
+    <h2 class="h-16 content-center bg-nw-blue-700 px-4 text-lg font-medium text-nw-blue-50">
+    Introduction</h2>
+    
+    
+    <div style="text-align:justify" class="p-3">The North Wyke Farm Platform and wider facilities has a 
+        long track record of supporting post graduate research projects. 
+        One of the purposes of the farm platform is to provide exceptional resources and data for academics from any discipline and location to use. 
+        Data is readily available and quality controlled meaning that projects can be initiated rapidly, with little cost.
+        You can access this data for your students or for your own study immediately and for free by registering on the 
+        
+        <a class="px-0 text-nw-blue-700 visited:text-amber-900 hover:text-orange-700 active:text-orange-900"
+        href="https://nwfp.rothamsted.ac.uk/">NWFP Data Portal.</a>
+        <br>
+        <br>
+        We are happy to discuss your ideas on projects and do anything that might be required to help. This can be anything form a conversation 
+        through to co-development of funding proposals.
+        
+    </div>
+    
+    <div class="p-3">
+        <ul>
+            <x-li-dot class="bg-nw-blue-500">Video testimonies (imperial, cambridge) and Photographs - add links here or add to page?</x-li-dot>
+            <x-li-dot class="bg-nw-blue-500">Student and staff accommodation</x-li-dot>
+            <x-li-dot class="bg-nw-blue-500">Student / Staff Support</x-li-dot>
+            <x-li-dot class="bg-nw-blue-500">Data orientation / explanation</x-li-dot>
+            <x-li-dot class="bg-nw-blue-500">Assistance with use of API's</x-li-dot>
+            <x-li-dot class="bg-nw-blue-500">Co-supervision / Mentoring (subject to collaboration agreement / funding)</x-li-dot>
+            <x-li-dot class="bg-nw-blue-500">Co-development of learning and teaching resources or data set publication</x-li-dot>
+            <x-li-dot class="bg-nw-blue-500">Support in the understanding of FAIR data and access to our Electronic Research Notebook (ERN)</x-li-dot>
+            <x-li-dot class="bg-nw-blue-500"> Hypothesis and Idea generation (link to student project ideas document)</x-li-dot>
+        </ul>
+        <br>
+</div>
+    
+<div class="p-1">
+    <div class="border-2 border-nw-blue-700 bg-nw-blue-50 p-3">
+        <p><strong>Please contact</strong>
+            <a onclick="window.open(this.href, '', 'resizable=yes,status=no,location=yes,toolbar=no,menubar=yes,fullscreen=no,scrollbars=yes,dependent=no'); return false;"href="mailto:nw.farmplatform@rothamsted.ac.uk">
+                <u>nw.farmplatform@rothamsted.ac.uk</u>
+            </a>for enquiries.
+        </p>
+    </div>
+</div>
+
+</x-guest-layout>
