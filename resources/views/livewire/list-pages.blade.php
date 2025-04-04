e23bb9496be401adce86526d319d281176cf792f
Nathalie Castells-Brooke <nathalie.castells@rothamsted.ac.uk>
2025-04-04 16:10:41 +0100

    home page style

--- resources/views/livewire/list-pages.blade.php
+++ resources/views/livewire/list-pages.blade.php
@@ -1,19 +1,10 @@
 <div>
 
-    <div class="w-100 container mx-auto flex flex-row justify-center gap-x-10  content-evenly place-content-between">
+    <div class="w-100 container mx-auto flex flex-row place-content-between content-evenly justify-center gap-x-10">
 
         @foreach ($pinned as $pin)
             <!-- card -->
-            <div class="mx-2 mt-4
-            flex
-            flex-col
-            aspect-square w-64 max-w-100
-            justify-between
-            overflow-hidden rounded-lg
-            border-l-4 border-orange-400
-             bg-gray-200 px-6 py-4
-             text-gray-800
-             shadow-xl"
+            <div class="max-w-100 mx-2 mt-4 flex aspect-square w-64 flex-col justify-between overflow-hidden rounded-lg border-l-4 border-orange-400 bg-gray-200 px-6 py-4 text-gray-800 shadow-xl hover:bg-orange-200 hover:shadow-md"
                 v-for="card in cards">
 
                 <div class="">
@@ -33,29 +24,29 @@
             </div><!--/ card-->
         @endforeach
     </div>
-<div class="row w-100 border border-b-8 border-nw-blue-700 mt-10"></div>
+    <div class="row w-100 mt-10 border border-b-8 border-nw-blue-700"></div>
     <div class="w-100 container mx-auto grid grid-cols-2 gap-4 p-3 lg:w-4/5">
 
         @foreach ($pages as $page)
             <!-- card -->
 
-            <div class="w-100 mx-2 mt-4 overflow-hidden rounded-lg border-l-4 border-nw-blue-800 bg-gray-200 shadow-xl md:flex-row"
+            <div class="max-w-100 mx-2 mt-4 flex  w-100 flex-col justify-between overflow-hidden rounded-lg border-l-4 border-nw-blue-700 bg-gray-200 px-6 py-4 text-gray-800 shadow-xl hover:bg-gray-400 hover:shadow-md"
                 v-for="card in cards">
-                <!-- media
-                <div class="w-full">
-                    <img class="inset-0 h-full w-full object-cover object-center" src="/images/banner1.png"/>
-                </div>
-                -->
-                <!-- content -->
-                <div class="flex w-full flex-col justify-between px-6 py-4 text-gray-800">
+
+                <div class="">
                     <h3 class="truncate text-lg font-semibold leading-tight">{{ $page->title }}</h3>
-                    <p class="mt-2 border-b-4 border-nw-blue-700 text-sm tracking-wide text-gray-700">
+                    <p class="mt-2 text-sm tracking-wide text-gray-700">
                         {!! $page->description !!}
                     </p>
-                    <p><a class="text-nw-blue-700 visited:text-amber-900 hover:text-orange-700 active:text-orange-900"
-                            href="{{ route('content.with.page', ['page' => $page->name]) }}">More ...</a>
                 </div>
-            </div><!--/ card-->
+                <div class="border-t-4 border-nw-blue-700 pt-5">
+
+                    <x-button-link class="btn-primary" href="{{ route('content.with.page', ['page' => $page->name]) }}">
+                        More...
+                    </x-button-link>
+
+                </div>
+            </div>
         @endforeach
     </div>
 </div>
