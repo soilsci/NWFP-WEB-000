29fdba5247488d8c8607e909dfbf36ee0bd7e1e1
Nathalie Castells-Brooke <nathalie.castells@rothamsted.ac.uk>
2025-05-29 13:19:09 +0100

    video_file to show page, link and also back button

--- resources/views/livewire/testimonies/show.blade.php
+++ resources/views/livewire/testimonies/show.blade.php
@@ -6,41 +6,56 @@
 
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('Student Testimony from '.  $student -> first_name . ' '  . $student -> last_name ) }}
+            {{ __('Student Testimony from ' . $student->first_name . ' ' . $student->last_name) }}
         </h2>
     </x-slot>
 
-    <div class="bg-nw-blue-900 text-center  font-medium text-3xl text-gray-50 p-10 ">
-        <div class="text-3xl"> {!! $testimony -> title !!}</div>
-        <div class="text-lg">{{ $student -> first_name . ' '  . $student -> last_name  }} - {{ $organisation -> name  }} </div>
+    <div class="bg-nw-blue-900 p-10 text-center text-3xl font-medium text-gray-50">
+        <div class="text-3xl"> {!! $testimony->title !!}</div>
+        <div class="text-lg">{{ $student->first_name . ' ' . $student->last_name }} - {{ $organisation->name }}
+        </div>
+        <x-button-link  href="{{ url()->previous() }}">Back</x-button-link>
     </div>
-    <div class="py-5 px-10 text-lg italic text-center ">{!! $testimony -> short !!}</div>
+    <div class="px-10 py-5 text-center text-lg italic">{!! $testimony->short !!}</div>
 
-    @if($testimony -> video_id)
-    <div class="flex flex-col items-center bg-gray-300 w-100">
-<div class="aspect-w-16 aspect-h-9">
-                    <iframe src="https://www.youtube.com/embed/{{ $testimony -> video_id }}" title="YouTube video player"
-                        width="400" height="225" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                </div>
-            </div>
-    @endif
-    @if ($testimony -> long)
-    <div class="py-3 px-3 border-t-2 border-nw-blue-900">{!! $testimony -> long !!}</div>
-    @endif
+    <div class="w-100 flex flex-col items-center bg-gray-300">
+        @if ($testimony->video_id)
+            <iframe class=" scale-100 transform overflow-hidden rounded-lg object-cover transition-transform"
+                src="https://www.youtube.com/embed/{{ $testimony->video_id }}" title="YouTube video player" width="400"
+                height="225" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        @elseif ($testimony->video_file)
+            <video class=" scale-100 transform overflow-hidden rounded-lg object-cover transition-transform"
+                controls muted>
+                <source src="/videos/{{ $testimony->video_file }}" type="video/mp4">
 
-        @if ($testimony -> video_id == null && $testimony -> URL)
-    <p class="p-2"><a class="text-nw-blue-700 visited:text-amber-900 hover:text-orange-700 active:text-orange-900" href="{{ $testimony -> URL }}">{{ $testimony -> URL }}</p>
-    @endif
-@if ($testimony ->project_id)
-    <div class="p-5 ">
-        <ul>
-            <x-li-arrow>Project : {{ $project -> name }} </x-li-arrow>
-
-        </ul>
+                Your browser does not support the video tag.
+            </video>
+        @else
+            <a href="{{ $testimony->URL }}"><img
+                    class="h-56 w-full scale-100 transform overflow-hidden rounded-lg object-cover transition-transform"
+                    src="https://picsum.photos/id/542/400/225" title="{{ $testimony->URL }}" width="400"
+                    height="225" frameborder="0" /></a>
+        @endif
     </div>
-@endif
 
+    @if ($testimony->long)
+        <div class="border-t-2 border-nw-blue-900 px-3 py-3">{!! $testimony->long !!}</div>
+    @endif
+
+    @if ($testimony->video_id == null && $testimony->URL)
+        <p class="p-2"><a
+                class="text-nw-blue-700 visited:text-amber-900 hover:text-orange-700 active:text-orange-900"
+                href="{{ $testimony->URL }}">{{ $testimony->URL }}</p>
+    @endif
+    @if ($testimony->project_id)
+        <div class="p-5">
+            <ul>
+                <x-li-arrow>Project : <a class=" text-nw-blue-700 visited:text-amber-900 hover:text-orange-700 active:text-orange-900" href="/projects/{{ $project->id }}">{{ $project->name }} </x-li-arrow>
+
+            </ul>
+        </div>
+    @endif
 
 </div>
