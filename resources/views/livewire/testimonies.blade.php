67314ffb5a2cd124f0bed2d68778af7363fa022a
Nathalie Castells-Brooke <nathalie.castells@rothamsted.ac.uk>
2025-05-29 13:06:32 +0100

    incorporated local video_file to livewire.testimonies

--- resources/views/livewire/testimonies.blade.php
+++ resources/views/livewire/testimonies.blade.php
@@ -4,72 +4,50 @@
             {{ __('Testimonies') }}
         </h2>
     </x-slot>
-    @php
-        $align = 'justify-items-end';
-    @endphp
-    @foreach ($testimonies as $testimony)
-        <!-- card -->
-        @php
-            if ($align == 'justify-items-start') {
-                $align = 'justify-items-end';
-            } else {
-                $align = 'justify-items-start';
-            }
-        @endphp
-        {{-- <div class="row {{ $align }} grid justify-items-start">
-            <div class="w-4/5  mx-2 m-4   border-2 border-nw-blue-700 bg-gray-200 px-6 py-4 text-gray-800 shadow-xl hover:bg-gray-400 hover:shadow-md"
-                v-for="card in cards">
-
-                <div class="">
-                    <h3 class="truncate text-lg font-semibold leading-tight">{{ $testimony->title }}</h3>
-                    <p class="mt-2 text-sm tracking-wide text-gray-700">
-                        {!! $testimony->short !!}
-                    </p>
-                </div>
-                <div class="pt-5">
-
-                    <x-button-link class="bn-primary" href="/testimonies/{{$testimony->id}}">
-                        More...
-                    </x-button-link>
-
-                </div>
-            </div>
-        </div> --}}
-        <div class="mx-auto mt-12">
+    <div class="mx-auto grid max-w-xs items-start gap-6  p-5 lg:max-w-none lg:grid-cols-3">
+        @foreach ($testimonies as $testimony)
+            <!-- card -->
             <div
-                class="flex items-center justify-start gap-3 overflow-hidden rounded-xl border border-nw-blue-700 bg-white">
+                class="border-nw-blue-700  bg-gray-200 hover:bg-gray-100  shadow-slate-950/5 flex h-full flex-col overflow-hidden rounded-2xl">
+                <!-- media -->
+                @if ($testimony->video_id)
+                    <iframe
+                        class="w-full scale-100 transform overflow-hidden rounded-lg object-cover transition-transform"
+                        src="https://www.youtube.com/embed/{{ $testimony->video_id }}" title="YouTube video player"
+                        width="400" height="225" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                @elseif ($testimony->video_file)
+                    <video
+                        class="w-full scale-100 transform overflow-hidden rounded-lg object-cover transition-transform"
+                        controls muted>
+                        <source src="/videos/{{ $testimony->video_file }}" type="video/mp4">
 
-                <div class="flex-shrink-0">
-                    @if ($testimony->video_id)
-                        <div class="aspect-w-16 aspect-h-9">
-                            <iframe src="https://www.youtube.com/embed/{{ $testimony->video_id }}"
-                                title="YouTube video player" width="400" height="225" frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                        </div>
-                    @else
-                        <div class="aspect-w-16 aspect-h-9">
-                            <a href="{{ $testimony->URL }}"><img src="https://picsum.photos/id/542/400/225" title="{{$testimony->URL  }}" width="400"
-                                height="225" frameborder="0" /></a>
-                        </div>
-                    @endif
+                        Your browser does not support the video tag.
+                    </video>
+                @else
+                    <a href="{{ $testimony->URL }}"><img
+                            class="h-56 w-full scale-100 transform overflow-hidden rounded-lg object-cover transition-transform"
+                            src="https://picsum.photos/id/542/400/225" title="{{ $testimony->URL }}" width="400"
+                            height="225" frameborder="0" /></a>
+                @endif
 
-                </div>
-
-                <div class="flex flex-col gap-2 p-2">
-
-                    <h3 class="truncate text-lg font-semibold leading-tight">{{ $testimony->title }}</h3>
-                    <p class="mt-2 text-sm tracking-wide text-gray-700">
-                        {!! $testimony->short !!}
-                    </p>
-
-                    <x-button-link class="bn-primary" href="/testimonies/{{ $testimony->id }}">
-                        More...
-                    </x-button-link>
+                <!-- content -->
+                <div class="flex flex-1 flex-col p-6">
+                    <div class="flex-1">
+                        <h3 class="truncate text-lg font-semibold leading-tight">{{ $testimony->title }}</h3>
+                        <p class="mt-2 text-sm tracking-wide text-gray-700">
+                            {!! $testimony->short !!}
+                        </p>
+                    </div>
+                    <div class="flex justify-end space-x-2">
+                        <x-button-link class="bn-primary mt-auto" href="/testimonies/{{ $testimony->id }}">
+                            More...
+                        </x-button-link>
+                    </div>
                 </div>
 
             </div>
-
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
