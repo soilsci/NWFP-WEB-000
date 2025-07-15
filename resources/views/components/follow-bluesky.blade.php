7b145644d022a74c7c5671abe9c8fb884cdaf0c4
soilsci <57556375+soilsci@users.noreply.github.com>
2025-07-15 11:17:40 +0100

    small edits and added images

--- resources/views/components/follow-bluesky.blade.php
+++ resources/views/components/follow-bluesky.blade.php
@@ -1,1 +1,1 @@
-<a href="https://bsky.app/profile/thefarmplatform.bsky.social"><img src="/logos/bluesky-logo-blue.svg" class="inline-block align middle"> Follow us on Bluesky</a
+<a href="https://bsky.app/profile/thefarmplatform.bsky.social" target="_top"><img src="/logos/bluesky-logo-blue.svg" class="inline-block align middle"> Follow us on Bluesky</a
